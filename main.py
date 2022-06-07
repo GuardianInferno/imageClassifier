393aec27bed4f20d2c8a0e31348104c4a8efe397
Hashir <hashirabbasi112@gmail.com>
2022-06-06 21:42:17 -0400

    model

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ import tensorflow as tf
 import numpy as np
 from tensorflow import keras
 import matplotlib.pyplot as plt
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+#print(train_labels[0])
+#print(train_images[0])
+
+plt.imshow(train_images[0], cmap='gray', vmin=0, vmax=255)
+plt.show()
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28,28)),
+])
